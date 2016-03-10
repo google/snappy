2b9152d9c5bed71dffb7f7f6c7a3ec48b058ff2d
Steinar H. Gunderson <sesse@google.com>
2016-03-10 18:37:05 +0100

    Default to glibtoolize instead of libtoolize if it exists, and also make it customizable through the environment variable $LIBTOOLIZE.
    Fixes autogen.sh issues on OS X, which ships its own
    (incompatible) libtoolize.
    
    R=jeff

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,11 @@
 rm -rf autom4te.cache
 aclocal -I m4
 autoheader
-libtoolize --copy
+if glibtoolize --version >/dev/null 2>/dev/null; then
+  LIBTOOLIZE=${LIBTOOLIZE:-glibtoolize}
+else
+  LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
+fi
+$LIBTOOLIZE --copy
 automake --add-missing --copy
 autoconf
