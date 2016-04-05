3af97d3579a1d7e3f910d2458482be341771d9b7
Soumitra Chatterjee <soumitra@hpe.com>
2016-04-06 00:05:38 +0530

    Support for AArch64

--- snappy-stubs-internal.h
+++ snappy-stubs-internal.h
@@ -96,9 +96,10 @@ static const int64 kint64max = static_cast<int64>(0x7FFFFFFFFFFFFFFFLL);
 
 // Potentially unaligned loads and stores.
 
-// x86 and PowerPC can simply do these loads and stores native.
+// x86, ARM64 and PowerPC can simply do these loads and stores native.
 
-#if defined(__i386__) || defined(__x86_64__) || defined(__powerpc__)
+#if defined(__i386__) || defined(__x86_64__) || defined(__powerpc__) \
+    || defined(__aarch64__)
 
 #define UNALIGNED_LOAD16(_p) (*reinterpret_cast<const uint16 *>(_p))
 #define UNALIGNED_LOAD32(_p) (*reinterpret_cast<const uint32 *>(_p))
