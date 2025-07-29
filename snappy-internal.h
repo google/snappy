758804a4bc359fccb75318568023f62999e38caa
anthony-zy <zheng.ziyang@zte.com.cn>
2025-07-29 14:08:43 +0800

    Use high-performance FindMatchLength to Optimize Snappy compression speed For RISC-V[skip ci]

--- snappy-internal.h
+++ snappy-internal.h
@@ -172,9 +172,10 @@ char* CompressFragment(const char* input,
 // loading from s2 + n.
 //
 // Separate implementation for 64-bit, little-endian cpus.
+// riscv and little-endian cpu choose this routinue can be done faster too.
 #if !SNAPPY_IS_BIG_ENDIAN && \
     (defined(__x86_64__) || defined(_M_X64) || defined(ARCH_PPC) || \
-     defined(ARCH_ARM))
+     defined(ARCH_ARM) || defined(__riscv))
 static inline std::pair<size_t, bool> FindMatchLength(const char* s1,
                                                       const char* s2,
                                                       const char* s2_limit,
